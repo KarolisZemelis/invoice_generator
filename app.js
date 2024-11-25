2c2d5725d07a91f98d65a6d07b0d0fb48c8c70d0
KarolisZemelis <karoliszemlis@gmail.com>
2024-11-25 11:30:59 +0200

    Added new function which finds where the data from geberateHtml function should be placed in HTML also added Buyer elements to HTML

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ function generateHtml(className, variableValue) {
   return `<div class="${className}">${variableValue}</div>`;
 }
 
+/**
+ * Function to find  DOM object with given class name and assign it to an aobject
+ * @param {string} className - The value of the className found in DOM
+ * @returns {variable} - Selected variable from DOM
+ */
+
+function findByClass(className) {
+  const domVariable = document.querySelector(`.${className}`);
+  return domVariable;
+}
+
 const url = "https://in3.dev/inv/";
 
 async function getData() {
@@ -29,7 +40,27 @@ async function getData() {
     const invoiceData = await getData();
     const buyerDetails = invoiceData.company.buyer;
     const buyerName = buyerDetails.name;
-    buyerDetailsHtml.innerHTML = generateHtml("buyerName", buyerName);
+    const buyerAddress = buyerDetails.address;
+    const buyerCode = buyerDetails.code;
+    const buyerVat = buyerDetails.vat;
+    const buyerPhone = buyerDetails.phone;
+    const buyerEmail = buyerDetails.email;
+    // console.log(findByClass("buyerAddress"));
+    findByClass("buyerName").innerHTML += generateHtml("buyerName", buyerName);
+    findByClass("buyerAddress").innerHTML += generateHtml(
+      "buyerAddress",
+      buyerAddress
+    );
+    findByClass("buyerCode").innerHTML += generateHtml("buyerCode", buyerCode);
+    findByClass("buyerVat").innerHTML += generateHtml("buyerVat", buyerVat);
+    findByClass("buyerPhone").innerHTML += generateHtml(
+      "buyerPhone",
+      buyerPhone
+    );
+    findByClass("buyerEmail").innerHTML += generateHtml(
+      "buyerEmail",
+      buyerEmail
+    );
   } catch (error) {
     console.error("Error fetching or processing data:", error);
   }
