85873ce300341af35731c328a38799dac4b7928d
KarolisZemelis <karoliszemlis@gmail.com>
2024-11-25 10:20:29 +0200

    getting data from API

---
+++ app.js
@@ -0,0 +1,20 @@
+const url = "https://in3.dev/inv/";
+
+async function getData() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status : ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+    throw error;
+  }
+}
+
+getData();
+
+const buyer = data.buyer;
+console.log(buyer);
